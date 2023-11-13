f09b296379f643fb658bf1a1d105363fad2462fd
IskXCr <IskXCr@outlook.com>
2023-11-13 18:40:44 +0800

    Fix typo

--- src/main.c
+++ src/main.c
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
         splc_trans_unit_list[i] = current_trans_unit;
 
         /* output error information */
-        SPLC_FDIAG("%d warning and %d errors generated.\n", current_trans_unit->warn_count, current_trans_unit->err_count);
+        SPLC_FDIAG("%d warning and %d errors generated.", current_trans_unit->warn_count, current_trans_unit->err_count);
 
         err_count = 0;
         warn_count = 0;
