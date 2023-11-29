ff5e94447c089fd87201eca4a76a7ea6129291a7
IskXCr <IskXCr@outlook.com>
2023-11-29 16:28:07 +0800

    Optimize build message

--- modules/splc/src/semantics.c
+++ modules/splc/src/semantics.c
@@ -71,5 +71,5 @@ int sem_test_typedef_name(const char *name)
 void sem_analyze(splc_trans_unit tunit)
 {
     // TODO(semantics): finish semantic analysis part
-    splcwarn_noloc("Semantic Analysis should be performed there.");
+    SPLC_FWARN_NOLOC("%s at %d: Semantic Analysis should be performed there.", __FILE__, __LINE__);
 }
--- modules/splc/src/splcpass.c
+++ modules/splc/src/splcpass.c
@@ -1,13 +1,17 @@
-#include "splcdef.h"
 #include "splcpass.h"
+#include "splcdef.h"
+#include "utils.h"
 
-void splc_run_single_passes(splc_single_pass* passes, splc_trans_unit tunit)
+void splc_run_single_passes(splc_single_pass *passes, splc_trans_unit tunit)
 {
     while (*passes != NULL)
     {
         (*passes)(tunit);
         if (err_count)
+        {
+            SPLC_FWARN_NOLOC("%s at line %d: failed to execute pass. The remaining passes are omitted.", __FILE__, __LINE__);
             break;
+        }
         ++passes;
     }
 }
