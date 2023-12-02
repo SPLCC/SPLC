1f2ca798ae67c060c836c4eefb71c9108b3a36cc
IskXCr <IskXCr@outlook.com>
2023-12-02 13:24:48 +0800

    Support ID copying in AST nodes

--- modules/splc/src/ast.c
+++ modules/splc/src/ast.c
@@ -191,9 +191,14 @@ ast_node ast_deep_copy(ast_node node)
     }
     if (SPLT_IS_VAL_ALLOCATED(node->type))
     {
+        SPLC_ASSERT(node->val != NULL);
         switch (node->type)
         {
         case SPLT_STR_UNIT:
+            result->val = (void *)strdup((char *)node->val);
+            SPLC_ALLOC_PTR_CHECK(result->val, "failed to copy string to another node");
+            break;
+        case SPLT_ID:
             result->val = (void *)strdup((char *)node->val);
             SPLC_ALLOC_PTR_CHECK(result->val, "failed to copy string to another node");
             break;
