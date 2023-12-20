ed9b7541d7c88c0b5e8d60dfddef4b2c1389167c
IskXCr <IskXCr@outlook.com>
2023-12-20 13:22:49 +0800

    Update logging

--- modules/splc/src/Translation/TranslationLogger.cc
+++ modules/splc/src/Translation/TranslationLogger.cc
@@ -8,75 +8,82 @@
 namespace splc {
 
 void printIndicator(std::ostream &os, utils::logging::Level level,
+                    size_t lineCnt, const std::string &lineStr, size_t start,
+                    size_t end)
+{
+    using namespace utils::logging;
+    using ControlSeq = utils::logging::ControlSeq;
+
+    // Sidebar: line counter
+    os << std::setw(8) << std::setfill(' ') << std::right << lineCnt << " | ";
+
+    // Actual content
+    os << lineStr.substr(0, start - 1);
+    os << ControlSeq::Bold << getLevelColor(level);
+    os << lineStr.substr(start - 1, end - start);
+    os << ControlSeq::Reset;
+    os << lineStr.substr(end - 1);
+    os << "\n";
+
+    // Indicator
+    os << "         | " << ControlSeq::Bold << getLevelColor(level);
+
+    // Space
+    for (size_t i = 1; i <= start - 1; ++i) {
+        os << " ";
+    }
+
+    // Actual indicator
+    os << "^";
+    for (size_t i = start + 1; i < end; ++i) {
+        os << "~";
+    }
+    os << ControlSeq::Reset;
+}
+
+void printIndicator(std::ostream &os, utils::logging::Level level,
                     const Location &loc)
 {
     using namespace utils::logging;
     using ControlSeq = utils::logging::ControlSeq;
 
     const std::string &filename = *loc.begin.contextName;
-    std::string line;
+    std::string lineStr;
 
     // Print a newline to separate from the previous string
     os << "\n";
-
-    // TODO(future): support multiple lines
-    if (std::ifstream in{filename}; in.good()) {
-        size_t lineCnt = 0;
-        while (in) {
-            std::getline(in, line);
-            lineCnt++;
-            if (lineCnt == loc.begin.line) {
-                break;
-            }
-        }
-        if (line.empty()) {
-            SPLC_LOG_ERROR(nullptr)
-                << "cannot retrieve line from location: " << ControlSeq::Bold
-                << loc << ControlSeq::Reset;
-        }
-        else {
-            // Print actual error
-            size_t start = loc.begin.column, end = 1;
-            if (loc.end.line > loc.begin.line) {
-                end = 1 + line.length();
-            }
-            else {
-                end = loc.end.column;
-            }
-
-            // Sidebar: line counter
-            os << std::setw(8) << std::setfill(' ') << std::right
-               << loc.begin.line << " | ";
-
-            // Actual content
-            os << line.substr(0, start - 1);
-            os << ControlSeq::Bold << getLevelColor(level);
-            os << line.substr(start - 1, end - start);
-            os << ControlSeq::Reset;
-            os << line.substr(end - 1);
-            os << "\n";
-
-            // Indicator
-            os << "         | " << ControlSeq::Bold << getLevelColor(level);
-
-            // Space
-            for (size_t i = 1; i <= start - 1; ++i) {
-                os << " ";
-            }
-
-            // Actual indicator
-            os << "^";
-            for (size_t i = start + 1; i < end; ++i) {
-                os << "~";
-            }
-            os << ControlSeq::Reset; 
-        }
-    }
-    else {
+    std::ifstream fin{filename};
+    if (!fin.good()) {
         os << ControlSeq::Bold << ControlSeq::Red
            << "cannot retrieve file: " << ControlSeq::Reset << ControlSeq::Bold
            << *loc.begin.contextName << ControlSeq::Reset;
+        return;
     }
+
+    size_t lineCnt = 0;
+    while (fin) {
+        std::getline(fin, lineStr);
+        lineCnt++;
+        if (lineCnt == loc.begin.line) {
+            break;
+        }
+    }
+    if (lineStr.empty()) {
+        SPLC_LOG_ERROR(nullptr)
+            << "cannot retrieve line from location: " << ControlSeq::Bold << loc
+            << ControlSeq::Reset;
+        return;
+    }
+
+    // Print actual error
+    size_t start = loc.begin.column, end = 1;
+    if (loc.end.line > loc.begin.line) {
+        end = 1 + lineStr.length();
+    }
+    else {
+        end = loc.end.column;
+    }
+    printIndicator(os, level, loc.begin.line, lineStr, start, end);
     // leave the remaining newline to `~Logger()`.
 }
 
